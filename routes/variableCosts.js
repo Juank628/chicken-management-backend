29f9d34de47f12d9b4ac8c6bb1bdf60a67901673
Juank628 <jsanchez_zunino@yahoo.com>
2021-03-02 23:29:49 -0500

    delete variableCost route

--- routes/variableCosts.js
+++ routes/variableCosts.js
@@ -3,22 +3,50 @@ const VariableCost = require("../models/VariableCost");
 
 const router = express.Router();
 
-router.get("/read", async (req, res, next) => {
-  try {
-    const data = await VariableCost.findAll();
-    res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500);
-    res.json(err);
-  }
-});
-
 router.post("/create", async (req, res, next) => {
   try {
     const newItem = await VariableCost.create(req.body);
     res.json(newItem);
   } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/read", async (req, res, next) => {
+  try {
+    const data = await VariableCost.findAll();
+    res.json(data);
+  } catch (err) {
+    res.status(500);
+    res.json(err);
+  }
+});
+
+router.post("/update", async (req, res, next) => {
+  const { id, description, unitType, unitSymbol, cost } = req.body;
+  try {
+    const [numberOfAffectedRows, affectedRows] = await VariableCost.update(
+      { description, unitType, unitSymbol, cost },
+      { where: { id } }
+    );
+    const data = await VariableCost.findByPk(id);
+    res.json(data);
+  } catch (err) {
+    res.status(500);
+    res.json(err);
+  }
+});
+
+router.delete("/delete", async (req, res, next) => {
+  const { id } = req.body;
+  try {
+    const result = await VariableCost.destroy({
+      where: { id },
+      force: true,
+    });
+    res.json(result);
+  } catch (err) {
+    res.status(500);
     res.json(err);
   }
 });
